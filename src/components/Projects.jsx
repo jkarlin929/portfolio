0249d0100b35bf14f21081723654b32ed38a4572
jkarlin929 <osida11572@gmail.com>
2018-06-18 16:27:40 -0400

    restructure grid

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,46 +8,28 @@ class Projects extends Component {
     <div className="citibikeProject">
       <Grid>
         <Row>
-          <Col xs={6} sm={6} md={6}>
+          <Col xs={12} md={6}>
           <h2>CitiBike Locator</h2>
             <a href="https://citibikers.herokuapp.com/" > <Image className="images" src="https://media.giphy.com/media/1Qn3B22t2gEbRNZC4g/giphy.gif" thumbnail responsive /> </a>
             <p>A CitiBike Locator using Google Maps API, Javascript on the front end, Node and Express on the back end.</p>
             <a href="https://citibikers.herokuapp.com/">|  CitiBike Locator |</a>
             <a href="https://github.com/jkarlin929/citibike">| <i class="fab fa-github"></i> Repository | </a>
           </Col>
-        </Row>
-      </Grid>
-    </div>
-    <div className="thankHeavenProject">
-      <Grid>
-        <Row>
-          <Col xs={6} md={6}>
+          <Col xs={12} md={6}>
           <h2>Thank Heaven: Children's Boutique</h2>
             <a href="https://thank-heaven.herokuapp.com"> <Image className="images" src="https://media.giphy.com/media/1wrljsSfgWp7NeZ2hl/giphy.gif" thumbnail responsive /> </a>
             <p> A group project created for a client using React, Webpack as a compiler, Express, Node, and Postgres. </p>
             <a href="https://thank-heaven.herokuapp.com"> | Thank Heaven: Children's Boutique |</a>
             <a href="https://github.com/jkarlin929/thank-heaven">|  <i class="fab fa-github"></i> Repository | </a>
           </Col>
-        </Row>
-      </Grid>
-    </div>
-    <div className="moonProject">
-      <Grid>
-        <Row>
-          <Col xs={6} md={6}>
+          <Col xs={12} md={6}>
           <h2>Astronomer's Guide</h2>
             <a href="https://astronobject.herokuapp.com/"> <Image className="images" src="https://media.giphy.com/media/23cXQxgrvG4eNeuJZF/giphy.gif" thumbnail responsive /> </a>
             <p>An Express with Node and Javascript Astronomy Web App. </p>
             <a href="https://astronobject.herokuapp.com/">  | Astronomer's Guide |</a>
             <a href="https://github.com/jkarlin929/moon_app">| <i class="fab fa-github"></i> Repository | </a>
           </Col>
-        </Row>
-      </Grid>
-    </div>
-    <div className="wordGame">
-      <Grid>
-        <Row>
-          <Col xs={6} md={6}>
+          <Col xs={12} md={6}>
           <h2>Word Guessing Game</h2>
             <a href="https://hellish-word-game.herokuapp.com/index.html"> <Image className="images" src="https://media.giphy.com/media/iB52Yt0VWdgDQ8FudX/giphy.gif" thumbnail responsive /> </a>
             <p>A word game built using HTML5, CSS and Javascript.</p>
