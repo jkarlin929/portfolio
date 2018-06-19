af9a48aca6ee49cbb719a3fe892aa1e6f90d43fe
jkarlin929 <osida11572@gmail.com>
2018-06-19 18:36:26 -0400

    edit colors and add icons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ class About extends Component {
   render() {
     return(
       <div className="about">
-        <p id="about">I am a <strong> Web Developer</strong> in NYC. I have experience with: Javascript <i class="fab fa-js-square"></i>, Node <i class="fab fa-node-js"></i>, React <i class="fab fa-react"></i>, HTML5 <i class="fab fa-html5"></i>, CSS3 <i class="fab fa-css3-alt"></i> and Express. <br/>My interest in solving problems persuaded me to become a mediator when working with others <br />and to apply my generosity to pursue personal evolution. <br /></p>
+        <p id="about">I am a <strong>Web Developer</strong> in NYC. I have experience with: Javascript <i class="fab fa-js-square"></i>, Node <i class="fab fa-node-js"></i>, React <i class="fab fa-react"></i>, HTML5 <i class="fab fa-html5"></i>, CSS3 <i class="fab fa-css3-alt"></i> and Express. <br/>My interest in solving problems persuaded me to become a mediator when working with others <br />and to apply my generosity to pursue personal evolution. <br /></p>
         <p id="projectsTitle">Projects</p>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,26 +13,26 @@ class Projects extends Component {
             <h2>Thank Heaven: Children's Boutique</h2>
             <a href="https://thank-heaven.herokuapp.com"> <Image className="images" src="https://media.giphy.com/media/1wrljsSfgWp7NeZ2hl/giphy.gif" thumbnail responsive /> </a>
             <p> A group project created for a client using React, Webpack as a compiler, Express, Node, and Postgres. </p>
-            <a href="https://thank-heaven.herokuapp.com"> | Thank Heaven: Children's Boutique | </a>
+            <a href="https://thank-heaven.herokuapp.com"> | <i class="fas fa-external-link-alt"></i> Thank Heaven: Children's Boutique | </a>
             <a href="https://github.com/jkarlin929/thank-heaven">|  <i class="fab fa-github"></i> Repository | </a>
             </Well>
           </Col>
           <Col xs={12} sm={6} md={6}>
           <Well bsSize="large">
-            <h2>CitiBike Locator</h2>
+            <h2><i class="fas fa-bicycle"></i> CitiBike Locator</h2>
               <a href="https://citibikers.herokuapp.com/" > <Image className="images" src="https://media.giphy.com/media/1Qn3B22t2gEbRNZC4g/giphy.gif" thumbnail responsive /> </a>
               <p>A CitiBike Locator using Google Maps API, Javascript on the front end, Node and Express on the back end.</p>
-              <a href="https://citibikers.herokuapp.com/">|  CitiBike Locator | </a>
+              <a href="https://citibikers.herokuapp.com/">| <i class="fas fa-external-link-alt"></i> CitiBike Locator | </a>
               <a href="https://github.com/jkarlin929/citibike">| <i class="fab fa-github"></i> Repository | </a>
             </Well>
           </Col>
           <Col xs={12} sm={6} md={6}>
           <Well bsSize="large">
-          <h2>Astronomer's Guide</h2>
+          <h2><i class="fas fa-user-astronaut"></i> Astronomer's Guide </h2>
             <a href="https://astronobject.herokuapp.com/"> <Image className="images" src="https://media.giphy.com/media/23cXQxgrvG4eNeuJZF/giphy.gif" thumbnail responsive /> </a>
             <p>An Express with Node and Javascript Astronomy Web App. </p>
             <br />
-            <a href="https://astronobject.herokuapp.com/">  | Astronomer's Guide | </a>
+            <a href="https://astronobject.herokuapp.com/"> | <i class="fas fa-external-link-alt"></i> Astronomer's Guide | </a>
             <a href="https://github.com/jkarlin929/moon_app">| <i class="fab fa-github"></i> Repository | </a>
             </Well>
           </Col>
@@ -41,7 +41,7 @@ class Projects extends Component {
           <h2>Word Guessing Game</h2>
             <a href="https://hellish-word-game.herokuapp.com/index.html"> <Image className="images" src="https://media.giphy.com/media/iB52Yt0VWdgDQ8FudX/giphy.gif" thumbnail responsive /> </a>
             <p>A word game built using HTML5, CSS and Javascript.</p>
-            <a href="https://hellish-word-game.herokuapp.com/index.html">| Word Game | </a>
+            <a href="https://hellish-word-game.herokuapp.com/index.html">| <i class="fas fa-external-link-alt"></i> Word Game | </a>
             <a href="https://github.com/jkarlin929/word-game">| <i class="fab fa-github"></i> Repository | </a>
             </Well>
           </Col>
@@ -49,9 +49,9 @@ class Projects extends Component {
       </Grid>
     </div>
         <div className="codepen">
-          <p>Codepens I enjoyed creating:</p>
-            <a href="https://codepen.io/jkarlin929/pen/dZLveB">| Color Switcher | </a>
-            <a href="https://codepen.io/jkarlin929/pen/mqoQKo">| Tic Tac Toe |</a>
+          <p> <i class="fab fa-codepen"></i> Codepens I enjoyed creating:</p>
+            <a href="https://codepen.io/jkarlin929/pen/dZLveB">| <i class="fab fa-codepen"></i> Color Switcher | </a>
+            <a href="https://codepen.io/jkarlin929/pen/mqoQKo">| <i class="fab fa-codepen"></i> Tic Tac Toe |</a>
         </div>
   </div>
     )
