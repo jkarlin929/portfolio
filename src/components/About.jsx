2f2f077d38cc39600907c207f6181d6714d0fd95
jkarlin929 <osida11572@gmail.com>
2019-01-08 21:55:49 -0500

    remove codepen, remove img  and add text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,8 +5,7 @@ class About extends Component {
   render() {
     return(
       <div className="about">
-        <Image id="photo" src="http://res.cloudinary.com/jkarlin929/image/upload/v1520994430/photoofme2_ysepvv.jpg" circle responsive />
-        <p id="about">I am a <strong>Web Developer</strong> in NYC. I have experience with: Javascript <i class="fab fa-js-square"></i>, Node <i class="fab fa-node-js"></i>, React <i class="fab fa-react"></i>, HTML5 <i class="fab fa-html5"></i>, CSS3 <i class="fab fa-css3-alt"></i> and Express. <br/>My interest in solving problems persuaded me to become a mediator when working with others <br />and to apply my generosity to pursue personal evolution. <br /></p>
+        <p id="about">I am a <strong>Web Developer</strong> in NYC. I have experience with: Javascript <i class="fab fa-js-square"></i>, Node.js <i class="fab fa-node-js"></i>, React <i class="fab fa-react"></i>, HTML5 <i class="fab fa-html5"></i>, CSS3 <i class="fab fa-css3-alt"></i> and Express. <br/>My interest in solving problems persuaded me to become a mediator when working with others <br />and to apply my generosity to pursue personal evolution. <br /></p>
         <Jumbotron id="projectsTitle">Projects</Jumbotron>
         </div>
     )
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Grid, Col, Row, Image, Well } from 'react-bootstrap';
 
 class Projects extends Component {
@@ -12,7 +12,7 @@ class Projects extends Component {
           <Well bsSize="small">
             <h2>Thank Heaven: Children's Boutique</h2>
             <a href="https://thank-heaven.herokuapp.com"> <Image className="images" src="https://media.giphy.com/media/1wrljsSfgWp7NeZ2hl/giphy.gif" thumbnail responsive /> </a>
-            <p> A group project created for a client using React, Webpack as a compiler, Express, Node, and Postgres. </p>
+            <p> A group project created for a client using React, Webpack, Express, Node.js, and Postgresql. </p>
             <a href="https://thank-heaven.herokuapp.com"> | <i class="fas fa-external-link-alt"></i> Thank Heaven: Children's Boutique | </a>
             <a href="https://github.com/jkarlin929/thank-heaven">|  <i class="fab fa-github"></i> Repository | </a>
             </Well>
@@ -21,7 +21,7 @@ class Projects extends Component {
           <Well bsSize="large">
             <h2><i class="fas fa-bicycle"></i> CitiBike Locator</h2>
               <a href="https://citibikers.herokuapp.com/" > <Image className="images" src="https://media.giphy.com/media/1Qn3B22t2gEbRNZC4g/giphy.gif" thumbnail responsive /> </a>
-              <p>A CitiBike Locator using Google Maps API, Javascript on the front end, Node and Express on the back end.</p>
+              <p>A CitiBike Locator using Google Maps API, Javascript on the front end, Node.js and Express on the back end.</p>
               <a href="https://citibikers.herokuapp.com/">| <i class="fas fa-external-link-alt"></i> CitiBike Locator | </a>
               <a href="https://github.com/jkarlin929/citibike">| <i class="fab fa-github"></i> Repository | </a>
             </Well>
@@ -30,7 +30,7 @@ class Projects extends Component {
           <Well bsSize="large">
           <h2><i class="fas fa-user-astronaut"></i> Astronomer's Guide </h2>
             <a href="https://astronobject.herokuapp.com/"> <Image className="images" src="https://media.giphy.com/media/23cXQxgrvG4eNeuJZF/giphy.gif" thumbnail responsive /> </a>
-            <p>An Express with Node and Javascript Astronomy Web App. </p>
+            <p>An Express with Node.js and Javascript Astronomy Web App. </p>
             <a href="https://astronobject.herokuapp.com/"> | <i class="fas fa-external-link-alt"></i> Astronomer's Guide | </a>
             <a href="https://github.com/jkarlin929/moon_app">| <i class="fab fa-github"></i> Repository | </a>
             </Well>
@@ -47,11 +47,6 @@ class Projects extends Component {
         </Row>
       </Grid>
     </div>
-        <div className="codepen">
-          <p> <i class="fab fa-codepen"></i> Codepens I enjoyed creating:</p>
-            <a href="https://codepen.io/jkarlin929/pen/dZLveB">| <i class="fab fa-codepen"></i> Color Switcher | </a>
-            <a href="https://codepen.io/jkarlin929/pen/mqoQKo">| <i class="fab fa-codepen"></i> Tic Tac Toe |</a>
-        </div>
   </div>
     )
   }
