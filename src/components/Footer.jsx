64ab9df7e1bbcb4735556d9512c3398706dc6e2d
jkarlin929 <osida11572@gmail.com>
2019-01-08 22:44:18 -0500

    edit colors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ class Footer extends Component {
   render() {
     return(
       <div className="footer" id="footer">
-        <p>Contact:  <a href="mailto:jkarlin929@gmail.com"> <i class="fas fa-envelope-square"></i> Gmail</a> <br /></p>
-        <p>Check me out on <a href="https://www.linkedin.com/in/jason-karlin/"> <i class="fab fa-linkedin"></i> LinkedIn </a></p>
+        <p>Contact  <a href="mailto:jkarlin929@gmail.com"> <i class="fas fa-envelope-square"></i> Gmail</a> <br /></p>
+        <p>Connect  <a href="https://www.linkedin.com/in/jason-karlin/"> <i class="fab fa-linkedin"></i> LinkedIn </a></p>
       </div>
     )
   }
