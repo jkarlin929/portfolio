401dd92975085443c75aeeae51182272a416de14
jkarlin929 <osida11572@gmail.com>
2019-02-06 15:11:47 -0500

    add margin and remove id of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return(
-      <div className="footer" id="footer">
+      <div className="footer">
         <p>Contact  <a href="mailto:jkarlin929@gmail.com"> <i className="fas fa-envelope-square"></i> Gmail</a> <br /></p>
         <p>Connect  <a href="https://www.linkedin.com/in/jason-karlin/"> <i className="fab fa-linkedin"></i> LinkedIn </a></p>
       </div>
