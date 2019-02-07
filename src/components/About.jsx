c118647a6dfd751131c604bb0f63fff030abaec6
jkarlin929 <osida11572@gmail.com>
2019-02-06 22:36:57 -0500

    edit css and change to functional component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
 
-
-class About extends Component {
-  render() {
-    return(
+const About = () => (
       <div className="about">
-        <p id="about">I am a <strong>Web Developer</strong> in NYC. I have experience with: Javascript <i className="fab fa-js-square"></i>, Node.js <i className="fab fa-node-js"></i>, React <i className="fab fa-react"></i>, HTML5 <i className="fab fa-html5"></i>, CSS3 <i className="fab fa-css3-alt"></i> and Express.</p>
+        <p id="about">I am a <strong>Web Developer</strong> from New York City. I have experience with: Javascript <i className="fab fa-js-square"></i>, Node.js <i className="fab fa-node-js"></i>, React <i className="fab fa-react"></i>, HTML5 <i className="fab fa-html5"></i>, CSS3 <i className="fab fa-css3-alt"></i> and Express.</p>
         </div>
     )
-  }
-}
+
 
 export default About;
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -12,7 +12,8 @@ class Projects extends Component {
           <Col xs={12}>
           <Well bsSize="small">
             <h2>Thank Heaven: Children's Boutique</h2>
-            <a href="https://thank-heaven.herokuapp.com"> <Image className="images" src="https://media.giphy.com/media/1wrljsSfgWp7NeZ2hl/giphy.gif" thumbnail responsive /> </a>
+            <a href="https://thank-heaven.herokuapp.com">
+            <Image className="images" src="https://media.giphy.com/media/1wrljsSfgWp7NeZ2hl/giphy.gif" thumbnail responsive /> </a>
             <p> A group project created for a client using React, Webpack, Express, Node.js, and Postgresql. </p>
             <a href="https://thank-heaven.herokuapp.com"> | <i className="fas fa-external-link-alt"></i> Thank Heaven: Children's Boutique | </a>
             <a href="https://github.com/jkarlin929/thank-heaven">|  <i className="fab fa-github"></i> Repository | </a>
@@ -20,19 +21,21 @@ class Projects extends Component {
           </Col>
           <Col xs={12} sm={6} md={6}>
           <Well bsSize="large">
-            <h2><i className="fas fa-bicycle"></i> CitiBike Locator</h2>
-              <a href="https://citibikers.herokuapp.com/" > <Image className="images" src="https://media.giphy.com/media/1Qn3B22t2gEbRNZC4g/giphy.gif" thumbnail responsive /> </a>
+            <h2>CitiBike Locator</h2>
+              <a href="https://citibikers.herokuapp.com/">
+              <Image className="images" src="https://media.giphy.com/media/1Qn3B22t2gEbRNZC4g/giphy.gif" thumbnail responsive /> </a>
               <p>A CitiBike Locator using Google Maps API, Javascript on the front end, Node.js and Express on the back end.</p>
-              <a href="https://citibikers.herokuapp.com/">| <i className="fas fa-external-link-alt"></i> CitiBike Locator | </a>
+              <a href="https://citibikers.herokuapp.com/">| <i className="fas fa-bicycle"></i> CitiBike Locator | </a>
               <a href="https://github.com/jkarlin929/citibike">| <i className="fab fa-github"></i> Repository | </a>
             </Well>
           </Col>
           <Col xs={12} sm={6} md={6}>
           <Well bsSize="large">
-          <h2><i className="fas fa-user-astronaut"></i> Astronomer's Guide </h2>
-            <a href="https://astronobject.herokuapp.com/"> <Image className="images" src="https://media.giphy.com/media/23cXQxgrvG4eNeuJZF/giphy.gif" thumbnail responsive /> </a>
+          <h2>Astronomer's Guide</h2>
+            <a href="https://astronobject.herokuapp.com/">
+            <Image className="images" src="https://media.giphy.com/media/23cXQxgrvG4eNeuJZF/giphy.gif" thumbnail responsive /> </a>
             <p>An Express with Node.js and Javascript Astronomy Web App. </p>
-            <a href="https://astronobject.herokuapp.com/"> | <i className="fas fa-external-link-alt"></i> Astronomer's Guide | </a>
+            <a href="https://astronobject.herokuapp.com/"> | <i className="fas fa-user-astronaut"></i> Astronomer's Guide | </a>
             <a href="https://github.com/jkarlin929/moon_app">| <i className="fab fa-github"></i> Repository | </a>
             </Well>
           </Col>
