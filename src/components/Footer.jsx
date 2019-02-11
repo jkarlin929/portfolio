669ffc40846d6bdd11003313dcb2cac2038afa24
jkarlin929 <osida11572@gmail.com>
2019-02-11 00:25:35 -0500

    edit footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return(
-      <div className="footer">
+      <div id="footer" className="footer">
         <p>Contact:  <a id="footer-contact" href="mailto:jkarlin929@gmail.com"> <i className="fas fa-envelope-square"></i> Gmail</a> <br /></p>
         <p>Connect:  <a id="footer-contact" href="https://www.linkedin.com/in/jason-karlin/"> <i className="fab fa-linkedin"></i> LinkedIn </a></p>
       </div>
