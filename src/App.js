a13de6f1092bd4796b4a8fb5907196de0f309b5f
jkarlin929 <osida11572@gmail.com>
2019-04-25 14:14:31 -0400

    burned it down

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import About from './components/About';
-import Topnav from './components/Topnav';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Topnav />
-        <About />
-        <Projects />
-        <Footer />
+      <div className="app">
+        <h1>Hello Friend</h1>
       </div>
     );
   }
