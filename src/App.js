50c64a7ea9c09646ba0344651d2fafaec27ce9e2
jkarlin929 <osida11572@gmail.com>
2019-04-25 15:21:58 -0400

    add basic outline of components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import './App.css';
+import About from './components/About';
+import Footer from './components/Footer';
+import Projects from './components/Projects';
+import Topnav from './components/Topnav';
 
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>Hello Friend</h1>
+        <Topnav />
+        <About />
+        <Projects />
+        <Footer />
       </div>
     );
   }
---
+++ src/components/Topnav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Topnav = () => (
+  <div>
+    <h1>Topnav</h1>
+  </div>
+)
+
+export default Topnav;
