ba4b1c657bd25d35b5d3d61197757368589e7a4a
jkarlin929 <osida11572@gmail.com>
2019-04-29 19:13:54 -0400

    edit contrast

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 const About = () => (
   <div className="about">
-    <img src="./assets/black-sand.jpg" alt=""/>
     <p className="about-desc"> Full Stack Web Developer </p>
   </div>
 )
