370e964fc255a6e6349100f8e52acb39636fcccc
jkarlin929 <osida11572@gmail.com>
2019-04-28 20:05:32 -0400

    add nav

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,9 @@ const Footer = () => (
   <div>
     <p>Contact:  <a id="footer-contact" href="mailto:jkarlin929@gmail.com"> <i className="fas fa-envelope-square"></i> Gmail</a> <br /></p>
     <p>Connect:  <a id="footer-contact" href="https://www.linkedin.com/in/jason-karlin/"> <i className="fab fa-linkedin"></i> LinkedIn </a></p>
+    <span>
+      Résumé
+    </span>
   </div>
 )
 
--- src/components/Topnav.jsx
+++ src/components/Topnav.jsx
@@ -13,9 +13,6 @@ const Topnav = () => (
         Projects
       </span>
       <span className="nav-item">
-        Résumé
-      </span>
-      <span className="nav-item">
         Contact
       </span>
     </nav>
