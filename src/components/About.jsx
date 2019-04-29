053d580565c0832641fbf7376404f72d2fa31671
jkarlin929 <osida11572@gmail.com>
2019-04-29 17:30:29 -0400

    add about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const About = () => (
-  <div>
-    <p className="about"> Jason Karlin Web Developer </p>
+  <div className="about">
+    <p> Full Stack Web Developer </p>
   </div>
 )
 
