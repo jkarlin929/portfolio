7e54d618dcace66959a5a83c5964d54129c69c71
jkarlin929 <osida11572@gmail.com>
2020-05-01 14:56:48 -0400

    added new styles

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,52 +1,56 @@
 import React from 'react';
 
 const Projects = () => (
-  <div>
-    <div>
+  <div className="container">
+    <div className="content-div">
       <h2>Thank Heaven: Children's Boutique</h2>
       <a href="https://thank-heaven.herokuapp.com">
-        <img className="images" src="https://media.giphy.com/media/1wrljsSfgWp7NeZ2hl/giphy.gif" />
+        <img className="images" alt="thank heaven" src="https://media.giphy.com/media/1wrljsSfgWp7NeZ2hl/giphy.gif" />
       </a>
-      <p> A group project created for a client using React, Webpack, Express, Node.js, and Postgresql. </p>
+      <p className="content-div-desc"> A group project created for a client using React, Webpack, Express, Node.js, and Postgresql. </p>
       <a href="https://thank-heaven.herokuapp.com"> | <i className="fas fa-external-link-alt"></i> Thank Heaven: Children's Boutique | </a>
       <a href="https://github.com/jkarlin929/thank-heaven">|  <i className="fab fa-github"></i> Repository | </a>
     </div>
-    <div>
+
+
+    <div className="content-div">
       <h2>CitiBike Locator</h2>
         <a href="https://citibikers.herokuapp.com/">
-          <img className="images" src="https://media.giphy.com/media/1Qn3B22t2gEbRNZC4g/giphy.gif" />
+          <img className="images" alt="citibike gif" src="https://media.giphy.com/media/1Qn3B22t2gEbRNZC4g/giphy.gif" />
         </a>
-          <p>A CitiBike Locator using Google Maps API, Javascript on the front end, Node.js and Express on the back end.</p>
+          <p className="content-div-desc">A CitiBike Locator using Google Maps API, JavaScript on the front end, Node.js and Express on the back end.</p>
         <a href="https://citibikers.herokuapp.com/">| <i className="fas fa-bicycle"></i> CitiBike Locator | </a>
         <a href="https://github.com/jkarlin929/citibike">| <i className="fab fa-github"></i> Repository | </a>
     </div>
 
-    <div>
+
+    <div className="content-div">
     <h2>Astronomer's Guide</h2>
       <a href="https://astronobject.herokuapp.com/">
-        <img className="images" src="https://media.giphy.com/media/23cXQxgrvG4eNeuJZF/giphy.gif" />
+        <img className="images" alt="astronomers gif" src="https://media.giphy.com/media/23cXQxgrvG4eNeuJZF/giphy.gif" />
       </a>
-      <p>An Express with Node.js and Javascript Astronomy Web App. </p>
+      <p className="content-div-desc">An Express with Node.js and JavaScript Astronomy Web App. </p>
       <a href="https://astronobject.herokuapp.com/"> | <i className="fas fa-user-astronaut"></i> Astronomer's Guide | </a>
       <a href="https://github.com/jkarlin929/moon_app">| <i className="fab fa-github"></i> Repository | </a>
       </div>
 
-    <div>
+
+    <div className="content-div">
     <h2>Word Guessing Game</h2>
       <a href="https://hellish-word-game.herokuapp.com/index.html">
-        <img className="images" src="https://media.giphy.com/media/iB52Yt0VWdgDQ8FudX/giphy.gif" />
+        <img className="images" alt="word guessing game gif" src="https://media.giphy.com/media/iB52Yt0VWdgDQ8FudX/giphy.gif" />
       </a>
-      <p>A word game built using HTML5, CSS and Javascript.</p>
+      <p className="content-div-desc">A word game built using HTML5, CSS3 and JavaScript.</p>
       <a href="https://hellish-word-game.herokuapp.com/index.html">| <i className="fas fa-external-link-alt"></i> Word Game | </a>
       <a href="https://github.com/jkarlin929/word-game">| <i className="fab fa-github"></i> Repository | </a>
     </div>
 
-    <div>
+    <div className="content-div">
     <h2>Grocery List</h2>
       <a href="https://rocky-escarpment-47175.herokuapp.com/">
-        <img className="images" src="https://media.giphy.com/media/oX7sukXJrig21asjv4/giphy.gif" />
+        <img className="images" alt="grocery list gif" src="https://media.giphy.com/media/oX7sukXJrig21asjv4/giphy.gif" />
       </a>
-      <p>CRUD app built using Ruby and Rails</p>
+      <p className="content-div-desc">A CRUD app built using Ruby and Rails</p>
       <a href="https://rocky-escarpment-47175.herokuapp.com/">| <i className="fas fa-shopping-cart"></i> Grocery List | </a>
       <a href="https://github.com/jkarlin929/grocery-list-rails-crud">| <i className="fab fa-github"></i> Repository | </a>
     </div>
